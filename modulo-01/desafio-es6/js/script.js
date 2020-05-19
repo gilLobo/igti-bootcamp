cefea980a3d54006ccd3c67d1c3268b618a51096
Gilberto Lobo <gilberto.lobo07@gmail.com>
2020-05-19 18:42:11 -0300

    Adicionando es6 no crud das aula anteriores

---
+++ modulo-01/desafio-es6/js/script.js
@@ -0,0 +1,96 @@
+var globalNames = ['Um', 'Dois', 'Tres'];
+var inputName = null;
+var currentIndex = null;
+var isEditing = false;
+
+window.addEventListener('load', () => {
+    inputName = document.querySelector('#inputName');
+    preventFormSubmit();
+    activateInput();
+    render();
+});
+
+function preventFormSubmit() {
+    function handleFormSubmit(event) {
+        event.preventDefault();
+    }
+    var form = document.querySelector('form');
+    form.addEventListener('submit', handleFormSubmit);
+}
+
+function activateInput() {
+    function insertName(newName) {
+        globalNames = [...globalNames, newName];
+    }
+
+    function updateName(newName) {
+        globalNames[currentIndex] = newName;
+    }
+
+    function handleTyping(event) {
+        if (event.key === 'Enter' && event.target.value.trim() !== '') {
+            if (!isEditing) {
+                insertName(event.target.value);
+            } else {
+                updateName(event.target.value);
+            }
+            render();
+            isEditing = false;
+        }
+    }
+
+    inputName.addEventListener('keyup', handleTyping);
+    inputName.focus();
+}
+
+function render() {
+    function createDeleteButton(index) {
+        function deleteName(){
+            globalNames = globalNames.filter((_, i) => i !== index );
+            render();
+        }
+        var button = document.createElement('button');
+        button.classList.add('deleteButton');
+        button.textContent = 'x';
+        button.addEventListener('click', deleteName);
+        return button;
+    }
+
+    function createSpan(name, index) {
+        function editItem(){
+            inputName.value = name;
+            inputName.focus();
+            currentIndex = index;
+            isEditing = true;
+        }
+
+        var span = document.createElement('span');
+        span.classList.add('clickable');
+        span.textContent = name;
+        span.addEventListener('click', editItem);
+
+        return span;
+    }
+
+    divNames = document.querySelector("#names");
+    divNames.innerHTML= '';
+    var ul = document.createElement('ul');
+
+    for(i=0; i < globalNames.length; i++) {
+        var currentName = globalNames[i];
+        var li = document.createElement('li');
+        var span = createSpan(currentName, i);
+        var button = createDeleteButton(i);
+
+        li.appendChild(button);
+        li.appendChild(span);
+        ul.appendChild(li)
+    }
+    divNames.appendChild(ul);
+    clearInput();
+}
+
+const clearInput = () => {
+    inputName.value='';
+    inputName.focus();
+};
